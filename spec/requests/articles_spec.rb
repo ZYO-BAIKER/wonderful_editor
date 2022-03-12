e57ffdb76f13920bdb5132fed7eb7464a69e65c4
ZYO-BAIKER <keisukeminakawa@gmail.com>
2022-03-13 03:07:22 +0900

    記事の詳細に関する API とテストを実装
    indexにもbodyを追加

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -14,8 +14,37 @@ RSpec.describe "Api::V1::Articles", type: :request do
       expect(res.length).to eq 3
       expect(response.status).to eq 200
       expect(res.map {|re| re["id"] }).to eq [article3.id, article1.id, article2.id]
-      expect(res.first.keys).to eq ["id", "title", "updated_at", "user"]
+      expect(res.first.keys).to eq ["id", "title", "body", "updated_at", "user"]
       expect(res.first["user"].keys).to eq ["id", "name", "updated_at"]
+    end
+  end
+
+  describe "GET /articles/:id" do
+    subject { get(api_v1_article_path(article_id)) } # ② subject のなかでarticle_id が呼ばれる。
+
+    context "指定したidのユーザーが存在する場合" do
+      let(:article_id) { article.id } # ③ article_id は article.id なので article が呼ばれる。
+      let(:article) { create(:article) } # ④ article が FactoryBot によってランダムに作られる。
+
+      it "特定のユーザーのレコードが取得できる" do
+        subject # ① it のなかで subject が呼ばれる。
+        res = JSON.parse(response.body)
+
+        expect(response.status).to eq 200 # ステータスコードが 200 であること
+        expect(res["id"]).to eq article.id
+        expect(res["title"]).to eq article.title
+        expect(res["body"]).to eq article.body
+        expect(res["updated_at"]).to be_present
+        expect(res["user"]["id"]).to eq article.user.id
+        expect(res["user"].keys).to eq ["id", "name", "updated_at"]
+      end
+    end
+
+    context "指定したidのユーザーが存在しないとき" do
+      let(:article_id){ 14234 } # ③ article_id 14234 が呼ばれる。
+      it "ユーザーが見つからない" do
+        expect { subject }.to raise_error ActiveRecord::RecordNotFound
+      end
     end
   end
 end
