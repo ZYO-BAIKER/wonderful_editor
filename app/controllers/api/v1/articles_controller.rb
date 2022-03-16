b91d06cd01e07b435b8d940a68623b29a2cb25e5
ZYO-BAIKER <keisukeminakawa@gmail.com>
2022-03-16 23:34:26 +0900

    記事の新規作成に関する API を実装

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -10,5 +10,17 @@ module Api::V1
       article = Article.find(params[:id])
       render json: article, serializer: Api::V1::ArticleSerializer
     end
+
+    def create
+      article = current_user.articles.create!(article_params)
+      # json として値を返す
+      render json: article, serializer: Api::V1::ArticleSerializer
+    end
+
+    private
+      # Only allow a list of trusted parameters through.
+      def article_params
+        params.permit(:title, :body)
+      end
   end
 end
