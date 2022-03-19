0247ca10a9a4a0f6622b27583975a82d13ea60ee
ZYO-BAIKER <keisukeminakawa@gmail.com>
2022-03-19 23:12:26 +0900

    ログアウトに関するテストを実装

--- spec/requests/api/v1/auth/sessions_spec.rb
+++ spec/requests/api/v1/auth/sessions_spec.rb
@@ -3,8 +3,8 @@ require "rails_helper"
 RSpec.describe "Api::V1::Auth::Sessions", type: :request do
   describe "POST /api/v1/auth/sign_in" do
     subject { post(api_v1_user_session_path, params: params) }
-
     let!(:user) { create(:user) }
+
     context "登録済のuser情報でログインする場合" do
       let(:params) { attributes_for(:user, email: user.email, password: user.password) }
 
@@ -50,6 +50,35 @@ RSpec.describe "Api::V1::Auth::Sessions", type: :request do
         expect(header["client"]).to be_blank
         expect(header["uid"]).to be_blank
         expect(header["token-type"]).to be_blank
+      end
+    end
+  end
+
+  describe "DELETE /api/v1/auth/sign_out" do
+    subject { delete(destroy_api_v1_user_session_path, params: headers) }
+    let(:user) { create(:user) }
+
+    context "適切なパラメーターを送信したとき" do
+      let(:headers) { user.create_new_auth_token }
+
+      it "ログアウトできる" do
+        subject
+        expect(headers["access-token"]).to be_present
+        expect(headers["token-type"]).to be_present
+        expect(headers["client"]).to be_present
+        expect(headers["expiry"]).to be_present
+        expect(headers["uid"]).to be_present
+      end
+    end
+
+    context "不適切なパラメーターを送信したとき" do
+      let(:headers) { { "access-token" => "faf", "token-type" => "tere", "client" => "faaaed", "expiry" => "gea", "uid" => "erw" } }
+
+      it "ログアウトできない" do
+        subject
+        res = JSON.parse(response.body)
+        expect(res["errors"]).to include "User was not found or was not logged in."
+        expect(response.status).to eq 404
       end
     end
   end
