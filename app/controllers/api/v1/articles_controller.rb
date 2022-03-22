1156ebfbd594ceaaa8555224bf714812645e24fd
ZYO-BAIKER <keisukeminakawa@gmail.com>
2022-03-22 12:04:07 +0900

    公開されている記事だけ取得できるように

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,28 +1,25 @@
 module Api::V1
-  # base_api_controller を継承
   class ArticlesController < BaseApiController
     before_action :authenticate_user!, only: [:create, :update, :destroy]
 
     def index
-      articles = Article.order(updated_at: :desc)
+      articles = Article.published.order(updated_at: :desc)
       render json: articles, each_serializer: Api::V1::ArticlePreviewSerializer
     end
 
     def show
-      article = Article.find(params[:id])
+      article = Article.published.find(params[:id])
       render json: article, serializer: Api::V1::ArticleSerializer
     end
 
     def create
       article = current_user.articles.create!(article_params)
-      # json として値を返す
       render json: article, serializer: Api::V1::ArticleSerializer
     end
 
     def update
       article = current_user.articles.find(params[:id])
       article.update!(article_params)
-      # json として値を返す
       render json: article, serializer: Api::V1::ArticleSerializer
     end
 
@@ -33,9 +30,8 @@ module Api::V1
 
     private
 
-      # Only allow a list of trusted parameters through.
       def article_params
-        params.require(:article).permit(:title, :body)
+        params.require(:article).permit(:title, :body, :status)
       end
   end
 end
